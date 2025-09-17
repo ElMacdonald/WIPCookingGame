f487c485a8005b715678786500fca330e9d37f59
KMacdonald <kylemacdo01@gmail.com>
2025-09-17 10:42:34 -0400

    Added functionality for reload

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,42 +1,73 @@
 using UnityEngine;
+using UnityEngine.System.Collections;
 
 public class Weapon : MonoBehaviour
 {
+    [Header("Weapon Stats")]
     public int dmg;
     public float firerate;
-    private float firerateTimer;
+    public float firerateTimer;
     public float range;
+    public float reloadTime;
+    public int magSize;
+    public int curMag;
+    public int reserveAmmo;
     private Transform cam;
+
+    [Header("Sound Effects")]
     public AudioSource[] audios;
+    private bool reloadInProgress;
+
 
 
 
     void Start()
     {
-
+        cam = Camera.main.transform;
     }
 
     void Update()
     {
-
+        firerateTimer += Time.deltaTime;
+        InputManager();
     }
 
     //Sends out a raycast and damages other player, overrideable by unconventional weapons
     private void Fire()
     {
-
+        if (curMag > 0)
+        {
+            if (Physics.Raycast(cam.position, cam.forward, out RaycastHit hit, range))
+            {
+                Debug.Log("Hit " + hit.transform.name);
+            }
+        }
     }
 
     //reloads the weapon
-    private void Reload()
+    private IEnumerator Reload()
     {
+        yield return new WaitForSeconds(reloadTime);
+        int neededAmmo = magSize - curMag;
 
+        if (reserveAmmo >= neededAmmo)
+        {
+            curMag += neededAmmo;
+            reserveAmmo -= neededAmmo;
+        }
+        else
+        {
+            curMag += reserveAmmo;
+            reserveAmmo = 0;
+        }
+
+        reloadInProgress = false;
     }
 
     //plays sound effect held within weapon code
-    private void PlaySound(AudioSource aud)
+    private IEnumerator PlaySound(AudioSource aud)
     {
-
+        yield return new WaitForSeconds(0.01f);
     }
 
     //Emulates weapon recoil
@@ -54,6 +85,17 @@ public class Weapon : MonoBehaviour
     //handles input from player
     private void InputManager()
     {
+        //Shooting
+        if (Input.GetAxis("Fire1") != 0 && firerate > firerateTimer)
+        {
+            Fire();
+        }
 
+        //Reloading
+        if (Input.GetKeyDown(KeyCode.R) && !reloadInProgress)
+        {
+            reloadInProgress = true;
+            Reload();
+        }
     }
 }
