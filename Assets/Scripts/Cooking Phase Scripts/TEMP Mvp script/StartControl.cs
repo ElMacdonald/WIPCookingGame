6ebe66f3a9096c82793cc4c95be25edd8f5e868e
KMacdonald <kylemacdo01@gmail.com>
2025-10-06 00:07:34 -0400

    Build Changes

--- Assets/Scripts/Cooking Phase Scripts/TEMP Mvp script/StartControl.cs
+++ Assets/Scripts/Cooking Phase Scripts/TEMP Mvp script/StartControl.cs
@@ -11,9 +11,19 @@ public class StartControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Ingredient Testing");
         }
     }
 }
